f78d7f461f594813377e29f897131b2c70dea1ef
Avanish Yadav <91160609+awanishyadav967@users.noreply.github.com>
2023-07-19 06:43:13 +0530

    Update Home.jsx

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
 
     try {
       //const response = await fetch('https://dalle-arbb.onrender.com/api/v1/post', {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://dall-e-bbtp.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
